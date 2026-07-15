465ebd5169529987f0d1e1c81ea44961799b5aba
SOL4R1S <mingh662@gmail.com>
2026-07-15 11:13:28 +0900

    001

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package com
+
+class Person(val name: String) {
+    fun greet() {
+        println("Hello, my name is $name")
+    }
+}
+
+fun main() {
+    val person = Person("Alice")
+    person.greet()
+}
